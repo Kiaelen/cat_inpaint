906b18228de62fa01fa6af2d2d6fdb8580840a2f
Kesdiael Ken <112612563+Kiaelen@users.noreply.github.com>
2025-08-07 11:16:35 +0800

    1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from diffusers import StableDiffusionInpaintPipeline
+from transformers import pipeline
 import torch
 from PIL import Image, ImageDraw
 import numpy as np
@@ -8,24 +9,34 @@ import os.path as path
 def draw_grid(image):
     plt.imshow(image)
     plt.grid(True)
-    plt.savefig("one-cat_axes")
+    plt.savefig("axes.png")
 
-ori_image = Image.open(path.join(path.expanduser("."), "one-cat.png")).convert("RGB")
+def get_depth(image):
+    pipe = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
+    depth = pipe(image)["depth"]
+    depth.save("depth.png")
+
+ori_image = Image.open(path.join(path.expanduser("."), "original.png")).convert("RGB")
 # Size: 2048 * 1152
 
+get_depth(ori_image)
+
 # Where cat should be
-crop_area = (0, 0, 512, 512)
+crop_area = [0, 0, 512, 512]
+mask_area = [100, 100, 356, 356] 
+
 image = ori_image.crop(crop_area)
 
 print(image.size)
-image.save("crop_img.png")
+image.save("cropped.png")
 
 mask = Image.new("L", image.size, 0)  # Start with all-black mask
 draw = ImageDraw.Draw(mask)
 
-# Mask
-sofa_area = (0, 0, 256, 256) 
-draw.rectangle(sofa_area, fill=255)
+for i in range(4):
+    mask_area[i] -= crop_area[i % 2]
+
+draw.rectangle(mask_area, fill=255)
 
 mask.save("mask.png")  # Optional: Save for verification
 
@@ -35,7 +46,7 @@ pipe = StableDiffusionInpaintPipeline.from_pretrained(
 )
 pipe = pipe.to("cuda")  # Use "cpu" if no GPU
 
-prompt = "an small black cat sitting on the shelf"  # Customize based on location (sofa/table)
+prompt = "an obscure small black cat sitting on the shelf"  # Customize based on location (sofa/table)
 negative_prompt = "blurry, deformed, ugly"  # Optional: Exclude unwanted artifacts
 
 output = pipe(
@@ -47,7 +58,7 @@ output = pipe(
     negative_prompt=negative_prompt,
 ).images[0].resize(image.size)
 
-output.save("living_room_with_cat.jpg")
+output.save("inpainted_cropped.jpg")
 
-ori_image.paste(output, crop_area[0], crop_area[2])
-ori_image.save("altered.png")
+ori_image.paste(output, (crop_area[0], crop_area[1]))
+ori_image.save("inpainted_original.png")
